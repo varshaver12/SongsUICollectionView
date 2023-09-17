3282d7436b6e846093a4c9f3f6fec33265d12e1b
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-09-17 12:30:57 +0300

    Initial Commit

---
+++ SongsUICollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SongsUICollectionView
+//
+//  Created by 1234 on 17.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
